106e4546c1ee7eb3f2c7eec5fb2ef07e1af9a221
samiran2076 <77327194+samiran2076@users.noreply.github.com>
2021-02-19 01:42:08 +0530

    Add files via upload

---
+++ aritmetic operations.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    float a,b,c,d,e,f,g,h,i,j,k;
+    // int a,b,c,d,e,f,g;
+    printf("enter the no.s:",a,b);
+    scanf("%f %f",&a,&b);
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=a/b;
+    h=a*a;
+    i=3.14*h;
+    j=2*3.14*a;
+    k=(a+a+b)/3;
+
+    //g=a%b;
+    printf("c=%f \nd=%f \ne=%f \nf=%f \nh=%f \ni=%f \nj=%f \nk=%f",c,d,e,f,h,i,j,k);
+    return 0;
+
+}
